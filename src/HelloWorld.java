99b4b6f5fc5aa05bb034e6273ca8aa48eeea567f
ablirysa <ysabellea@nycstudents.net>
2022-09-28 08:34:59 -0400

    added three variables and changed the printed statements

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,2 +1,11 @@
-package PACKAGE_NAME;public class HelloWorld {
+public class HelloWorld {
+    public static void main(String[] args) {
+        int favNum = 5;
+        String dogName = "Bruno";
+        double costOfCookies = 2.25;
+
+        System.out.println("My favorite number is " + favNum);
+        System.out.print("While, my dog's name is " + dogName);
+        System.out.print(" and I love eating the cookies in the vending machine that cost $" + costOfCookies);
+    }
 }
